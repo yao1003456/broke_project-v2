ebd8598a4fbdcef20291e8e1948ec2a654ff91b7
yao1003456 <stampyyao@gmail.com>
2024-01-23 14:59:01 +0000

    C3

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q$s(@2p76ytwxqcqv8)hxe9w#=39kphj-pah&v01qk)!_dy2%1'
+SECRET_KEY = '90ixmsaiy-@9c!ic9arkekgtj99jr2__d@-f=(y#=30uvp#9*9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango'
 ]
 
 MIDDLEWARE = [
